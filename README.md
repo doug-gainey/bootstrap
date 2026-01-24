c5cc53263b2639700a82d5175f5166a51f194ec2
Doug Gainey <douggainey@gmail.com>
2026-01-23 21:51:29 -0500

    Tweaks.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Bootstrap
+# Machine Bootstrap Files
+
+* [Powershell Profile](machine-bootstrap/powershell/Microsoft.PowerShell_profile.ps1) => /Users/{User}/Documents/PowerShell
+
+* [Oh My Posh Theme](machine-bootstrap/oh-my-posh/web.omp.json) => /Users/{User}/.config/oh-my-posh
+
+* [Git Config](machine-bootstrap/git/.gitconfig) => /Users/{User}
+
+# Project Bootstrap Files
 Standard configs, templates, and helpers to quickly bootstrap personal projects.
---
+++ machine-bootstrap/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,28 @@
+# ---------- Core UX ----------
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+# ---------- Oh My Posh ----------
+if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
+    oh-my-posh init pwsh --config "$HOME\.config\oh-my-posh\web.omp.json" | Invoke-Expression
+}
+
+# ---------- Icons ----------
+if (Get-Module -ListAvailable Terminal-Icons) {
+    Import-Module Terminal-Icons
+}
+
+# ---------- Navigation ----------
+if (Get-Module -ListAvailable z) {
+    Import-Module z
+}
+
+# ---------- Dev Aliases ----------
+Set-Alias g git
+Set-Alias v code
+Set-Alias n npm
+Set-Alias pn pnpm
+
+# ---------- Safety ----------
+Set-PSReadLineKeyHandler -Key Ctrl+c -Function Copy
